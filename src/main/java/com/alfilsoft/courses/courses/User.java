240f42c1652ac39ab8c2921441b960d157202e87
Juan Camilo Martinez Cortés <juancamilomartinez727@gmail.com>
2023-11-30 08:19:56 -0500

    adding atributtes: email, description category, code, valutation and price

--- src/main/java/com/alfilsoft/courses/courses/User.java
+++ src/main/java/com/alfilsoft/courses/courses/User.java
@@ -4,4 +4,10 @@ public class User {
 
     private long id;
     private String name;
+    private String email;
+    private String description;
+    private String code;
+    private String category;
+    private Int valuation;
+    private double price;
 }
