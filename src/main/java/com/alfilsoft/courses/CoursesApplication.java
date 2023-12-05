a196b157d232ace69ec00c75bf4a8f55b2100eb4
Juan Camilo Martinez Cortés <juancamilomartinez727@gmail.com>
2023-12-05 08:22:47 -0500

    New service 'rest'

--- src/main/java/com/alfilsoft/courses/CoursesApplication.java
+++ src/main/java/com/alfilsoft/courses/CoursesApplication.java
@@ -2,6 +2,28 @@ package com.alfilsoft.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MiControlador {
+
+    @GetMapping("/saludo")
+    public String saludar() {
+        return "Hola, este es un servicio REST con Spring Boot";
+    }
+
+    @PostMapping("/crear")
+    public String crearAlgo(@RequestParam String nombre) {
+        // Lógica para crear algo con el nombre proporcionado
+        return "Se creó algo con el nombre: " + nombre;
+    }
+}
+
 
 @SpringBootApplication
 public class CoursesApplication {
